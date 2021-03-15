f133c0a32436fa26d2cc7e876a55d8e844cdbd59
Ashish Arora <ashish007722@gmail.com>
2021-03-15 22:57:15 +0530

    Navigation graph and List,Add,Update Fragments
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -0,0 +1,16 @@
+package com.example.todoapp.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todoapp.R
+
+class ListFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+}
