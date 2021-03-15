1b7f4a4ae77aad2bd365aad6df8a5dff4ce36513
Ashish Arora <ashish007722@gmail.com>
2021-03-16 04:29:52 +0530

    Priority data modle typeconverter this is used by RoomDB to convert Priority Type to string as the column in sql table can only of primitive types
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ app/src/main/java/com/example/todoapp/data/Converter.kt
@@ -0,0 +1,16 @@
+package com.example.todoapp.data
+
+import androidx.room.TypeConverter
+
+class Converter {
+
+    @TypeConverter
+    fun fromPriority(priority: Priority): String {
+        return priority.name
+    }
+
+    @TypeConverter
+    fun toPriority(priority: String): Priority {
+        return Priority.valueOf(priority)
+    }
+}
--- app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+@TypeConverters(Converter::class)
 abstract class ToDoDatabase: RoomDatabase() {
 
     abstract fun todoDao(): ToDoDao
