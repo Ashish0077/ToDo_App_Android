aa87c4d5f37b867e983c7d0eabb232f260a93594
Ashish Arora <ashish007722@gmail.com>
2021-03-16 04:00:17 +0530

    RoomDB Entity data class defined
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ app/src/main/java/com/example/todoapp/data/ToDoData.kt
@@ -0,0 +1,13 @@
+package com.example.todoapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String
+)
