cdfd4f0b608393c805064c6bd5b1981d73194c60
Ashish Arora <ashish007722@gmail.com>
2021-03-16 04:05:44 +0530

    RoomDB Database Access Object(DAO) interface defined
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ app/src/main/java/com/example/todoapp/data/ToDoDao.kt
@@ -0,0 +1,17 @@
+package com.example.todoapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<ToDoData>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(toDoData: ToDoData)
+}
