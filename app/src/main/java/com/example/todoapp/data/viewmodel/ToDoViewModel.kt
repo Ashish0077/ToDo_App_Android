297d535f9ca00929f1fbc19d7a2821cff4ab2132
Ashish Arora <ashish007722@gmail.com>
2021-03-16 17:42:31 +0530

    ToDoViewModel
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ app/src/main/java/com/example/todoapp/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.todoapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.data.ToDoDatabase
+import com.example.todoapp.data.models.ToDoData
+import com.example.todoapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application): AndroidViewModel(application) {
+    private val todoDao = ToDoDatabase.getDatabase(application).todoDao()
+    private val repository: ToDoRepository
+    private val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(todoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+}
